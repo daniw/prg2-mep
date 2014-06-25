8429acecf781d8b36e2b0b86fab21d136fa45c1a
daniw <dani.winz@bluewin.ch>
2014-06-25 11:42:56 +0200

    Skript für Konvertierung markdown -> html

---
+++ make.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+###############################################################################
+### Script to convert markdown files to html files and change links 
+### to the corresponding html files. 
+###
+### dependencies: 
+###     pandoc
+###############################################################################
+
+# change to current directory
+cd $PWD
+
+# create folder for html files
+mkdir html
+for i in [0-9]*/; do mkdir html/$i; done
+
+# convert files from markdown to html
+pandoc README.md > html/index.html
+sed -i -e 's/.md/.md.html/g' html/index.html
+
+for i in [0-9]*/*.md; do pandoc $i > html/$i.html; sed -i -e 's/.md/.md.html/g' html/$i.html; done
+
