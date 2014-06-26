946ff7cd96b707ddac4b3f1bab5cbf27b0a3c7d6
daniw <dani.winz@bluewin.ch>
2014-06-26 18:14:38 +0200

    Fragen in Script ergänzt

--- make.sh
+++ make.sh
@@ -20,6 +20,8 @@ pandoc -s README.md > html/index.md.html
 sed -i -e 's/.md/.md.html/g' html/index.md.html
 pandoc -s whereis.md > html/whereis.md.html
 sed -i -e 's/.md/.md.html/g' -e 's/README/index/g' html/whereis.md.html
+pandoc -s fragen.md > html/fragen.md.html
+sed -i -e 's/.md/.md.html/g' -e 's/README/index/g' html/fragen.md.html
 
 for i in [0-9]*/*.md; do pandoc -s $i > html/$i.html; sed -i -e 's/.md/.md.html/g' -e 's/README/index/g' html/$i.html; done
 
